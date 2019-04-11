aaf53ba4c468dfc387b0cd890fc9c4e958bc268c
Naoufal YASSIRI <44404286+NFLgunCode@users.noreply.github.com>
2019-04-11 23:32:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,136 @@
+
+
+
+
+
+function try1(){ 
+    
+    if (event.which==65){
+     
+    var btn=document.querySelectorAll(".key")[0];
+    btn.style.border="0.1rem solid orange";
+    btn.style.boxShadow="0px 0px 15px yellow";
+    document.getElementsByTagName("audio")[0].play();
+
+        function stp (){
+            btn.style.border="0.1rem solid black";
+            btn.style.boxShadow="none";
+
+
+        }
+        setTimeout(stp,300);
+    }
+            
+    else if (event.which== 83) {
+        var btn=document.querySelectorAll(".key")[1];
+        btn.style.border= "0.1rem solid orange";
+        btn.style.boxShadow="0px 0px 15px yellow";
+        document.getElementsByTagName("audio")[1].play();
+
+        function stp (){
+            btn.style.border= "0.1rem solid black";
+            btn.style.boxShadow= "none";
+        }
+        setTimeout(stp,300);
+
+    }
+    else if (event.which== 68) {
+        var btn=document.querySelectorAll(".key")[2];
+        btn.style.border= "0.1rem solid orange";
+        btn.style.boxShadow="0px 0px 15px yellow";
+        document.getElementsByTagName("audio")[2].play();
+
+        function stp (){
+            btn.style.border= "0.1rem solid black";
+            btn.style.boxShadow= "none";
+        }
+        setTimeout(stp,300);
+
+    }
+    else if (event.which== 70) {
+        var btn=document.querySelectorAll(".key")[3];
+        btn.style.border= "0.1rem solid orange";
+        btn.style.boxShadow="0px 0px 15px yellow";
+        document.getElementsByTagName("audio")[3].play();
+
+        function stp (){
+            btn.style.border= "0.1rem solid black";
+            btn.style.boxShadow= "none";
+        }
+        setTimeout(stp,300);
+
+
+    }
+
+    else if (event.which== 71) {
+        var btn=document.querySelectorAll(".key")[4];
+        btn.style.border= "0.1rem solid orange";
+        btn.style.boxShadow="0px 0px 15px yellow";
+        document.getElementsByTagName("audio")[4].play();
+
+        function stp (){
+            btn.style.border= "0.1rem solid black";
+            btn.style.boxShadow= "none";
+        }
+        setTimeout(stp,300);
+
+    }
+    else if (event.which== 72) {
+        var btn=document.querySelectorAll(".key")[5];
+        btn.style.border= "0.1rem solid orange";
+        btn.style.boxShadow="0px 0px 15px yellow";
+        document.getElementsByTagName("audio")[5].play();
+
+        function stp (){
+            btn.style.border= "0.1rem solid black";
+            btn.style.boxShadow= "none";
+        }
+        setTimeout(stp,300);
+
+    }
+    else if (event.which== 74) {
+        var btn=document.querySelectorAll(".key")[6];
+        btn.style.border= "0.1rem solid orange";
+        btn.style.boxShadow="0px 0px 15px yellow";
+        document.getElementsByTagName("audio")[6].play();
+
+        function stp (){
+            btn.style.border= "0.1rem solid black";
+            btn.style.boxShadow= "none";
+        }
+        setTimeout(stp,300);
+
+    }
+    else if (event.which== 75) {
+        var btn=document.querySelectorAll(".key")[7];
+        btn.style.border= "0.1rem solid orange";
+        btn.style.boxShadow="0px 0px 15px yellow";
+        document.getElementsByTagName("audio")[7].play();
+
+        function stp (){
+            btn.style.border= "0.1rem solid black";
+            btn.style.boxShadow= "none";
+        }
+        setTimeout(stp,300);
+
+    }
+    else if (event.which== 76) {
+        var btn=document.querySelectorAll(".key")[8];
+        btn.style.border= "0.1rem solid orange";
+        btn.style.boxShadow="0px 0px 15px yellow";
+        document.getElementsByTagName("audio")[8].play();
+
+        function stp (){
+            btn.style.border= "0.1rem solid black";
+            btn.style.boxShadow= "none";
+        }
+        setTimeout(stp,300);
+
+    }
+   
+
+}
+
+
+
+
